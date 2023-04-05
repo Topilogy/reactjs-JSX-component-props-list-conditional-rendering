187367056e7de2f48b1635908140288829268da1
topilogy <topilogy@gmail.com>
2023-04-05 13:34:25 +0100

    Conditional Renderind

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"> 
-      <User name = "Temitope" age = {15} email = "topilogy@gmail.com" />
-      <User name = "Funmilayo" age = {10} email = "funmi@gmail.com" />
-      <User name = "Dupe" age = {12} email = "dupe@gmail.com" /> 
-    </div>
-  );
+  const age = 15;
+  if (age >= 18) {
+    return (
+      <h1>OVER AGE</h1>
+    );
+  }
+  else {
+    return (
+      <div className = "App">
+        <h1>UNDER AGE</h1>
+      </div>
+    )
+  }
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h1> {props.name} </h1>
-      <h1> {props.age} </h1>
-      <h1> {props.email} </h1>
-    </div>
-  );
-};
 
 
 export default App;
