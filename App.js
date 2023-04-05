966fdc2e41aee779c25777968d6f56f8252f7a99
topilogy <topilogy@gmail.com>
2023-04-05 13:25:07 +0100

    JSX, Component and Props

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [showText, setShowText] = useState("false")
-
   return (
-    <div className='App'>
-      <button onClick={ () => {
-        setShowText(!showText)
-      }}
-      >
-        Show/Hide
-      </button>
-      {showText &&<h1>Hey, how are you</h1>}
+    <div className="App"> 
+      <User name = "Temitope" age = {15} email = "topilogy@gmail.com" />
+      <User name = "Funmilayo" age = {10} email = "funmi@gmail.com" />
+      <User name = "Dupe" age = {12} email = "dupe@gmail.com" /> 
     </div>
-  )
+  );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1> {props.name} </h1>
+      <h1> {props.age} </h1>
+      <h1> {props.email} </h1>
+    </div>
+  );
+};
 
 
 export default App;
